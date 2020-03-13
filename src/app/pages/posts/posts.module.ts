ffc84b3be0ac9651287de2818ab0cba64cf371ca
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-13 12:37:17 -0600

    26. @Input - Enviar información hacia un componente hijo

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    PostsComponent
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     CommonModule,
